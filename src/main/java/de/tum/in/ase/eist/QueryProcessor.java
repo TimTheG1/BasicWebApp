8d68c52735b572980446854c7ab77b13dfff7da4
ge43nib <ge43nib@mytum.de>
2022-07-13 11:12:53 +0200

    Largest number

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -14,7 +14,7 @@ public class QueryProcessor {
         } else if (query.contains("name")) {
            return "MyTeam";
         } else { // TODO extend the programm here
-            return "20735";
+            return "20557";
         }
     }
 }
